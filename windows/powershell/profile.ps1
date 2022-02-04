1525b3660fc0a59d2e774191142c09490c1f8ba5
Mukhlis Akbarrudin <mukhlis3389@gmail.com>
2022-02-04 20:58:15 +0700

    feat(windows): use pure theme ohmyposh

--- windows/powershell/profile.ps1
+++ windows/powershell/profile.ps1
@@ -1,5 +1,5 @@
 #oh-my-posh
-oh-my-posh --init --shell pwsh --config ~\AppData\Local\Programs\oh-my-posh\themes\robbyrussel.omp.json | Invoke-Expression
+oh-my-posh --init --shell pwsh --config ~\AppData\Local\Programs\oh-my-posh\themes\pure.omp.json | Invoke-Expression
 
 #git-aliases
 Import-Module git-aliases -DisableNameChecking
